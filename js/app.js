662bafd8876f7d9a47f7ad1b7e6c12c7ce266f9a
root <eli.dungan@gmail.com>
2019-07-18 14:58:13 -0700

    completed feature1

--- js/app.js
+++ js/app.js
@@ -1,20 +1,55 @@
 'use-strict'
 
-/**-----global variables */
+/* -----TODO:-----
+Use AJAX, specifically $.get(), to read the provided JSON file.
+Each object should become a new instance of a constructor function. Refer to the data to determine the necessary properties.
+Use jQuery to make a copy of the HTML template of the photo component. For each object, fill in the duplicated template with its properties, then append the copy to the DOM. */
 
-/** -----objects----- */
 
-function Img(imageUrl, title, description, keyword, horns) {
-  this.imageUrl = imageUrl;
-  this.title = title;
-  this.description = description;
-  this.keyword = keyword;
-  this.horns = horns;
+/* -----global variables----- */
+
+/* -----objects----- */
+
+function Img(img) {
+  this.image_url = img.image_url;
+  this.title = img.title;
+  this.description = img.description;
+  this.keyword = img.keyword;
+  this.horns = img.horns;
 }
 
-// TODO:
-// Use AJAX, specifically $.get(), to read the provided JSON file.
-// Each object should become a new instance of a constructor function. Refer to the data to determine the necessary properties.
-// Use jQuery to make a copy of the HTML template of the photo component. For each object, fill in the duplicated template with its properties, then append the copy to the DOM.
+Img.allImgs = [];
 
-$.get();
+Img.prototype.render = function() {
+  $('main').append('<div class="clone"></div>'); // appends html to the main element
+  let imgClone = $('div[class="clone"]'); //
+
+  let imgHtml = $('#photo-template').html(); //
+
+  imgClone.html(imgHtml);
+
+  imgClone.find('h2').text(this.title);
+  imgClone.find('img').attr('src', this.image_url);
+  imgClone.find('p').text(this.description);
+  imgClone.removeClass('clone');
+  imgClone.attr('class', this.keyword);
+}
+
+Img.readJson = () => {
+  $.get('https://elijah-dungan.github.io/data/page-1.json', 'json') // gets json data
+    .then(data => { // data is the parameter of the function
+      data.forEach(item => { // creates array from the data and uses forEach
+        Img.allImgs.push(new Img(item)); // a new instance based on the array is created
+      })
+    })
+    .then(Img.loadImgs);
+}
+
+Img.loadImgs = () => {
+  Img.allImgs.forEach(img => img.render());
+}
+
+/* -----executables----- */
+
+$(() => Img.readJson());
+console.log(Img.allImgs);
